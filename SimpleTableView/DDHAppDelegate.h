08f4444e3191de1fab75b3f679f97344b6b85988
Dominik Hauser <dominik.hauser@appseleration.com>
2014-04-02 14:43:04 +0200

    Initial Commit

---
+++ SimpleTableView/DDHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDHAppDelegate.h
+//  SimpleTableView
+//
+//  Created by Dominik Hauser on 02.04.14.
+//  Copyright (c) 2014 dasdom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
